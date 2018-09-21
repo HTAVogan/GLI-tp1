7f060196024bb917367933092bf285966b7162c6
Unknown <jbbordier@hotmail.fr>
2018-09-21 09:17:49 +0200

    getter setter

--- src/model/Item.java
+++ src/model/Item.java
@@ -4,4 +4,22 @@ public class Item {
 	private String intitule;
 	private String desc;
 	private float value;
+	public String getIntitule() {
+		return intitule;
+	}
+	public void setIntitule(String intitule) {
+		this.intitule = intitule;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public float getValue() {
+		return value;
+	}
+	public void setValue(float value) {
+		this.value = value;
+	}
 }
