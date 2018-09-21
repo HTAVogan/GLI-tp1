06b9b441b4624307135fb3a637c4cf63e4dd7508
Dagaubs <alan181096@gmail.com>
2018-09-21 08:46:30 +0200

    creation of package model

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,5 +1,7 @@
 package model;
 
 public class Item {
-
+	private String intitule;
+	private String desc;
+	private float value;
 }
--- src/model/Model.java
+++ src/model/Model.java
@@ -1,5 +1,19 @@
 package model;
 
-public class Model {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Model {
+	private String title;
+	private List<Item> items;
+	
+	public Model(String title) {
+		this.title = title;
+		this.items = new ArrayList<Item>();
+	}
+	
+	public Model(String title, List<Item> items) {
+		this.title = title;
+		this.items = items;
+	}
 }
